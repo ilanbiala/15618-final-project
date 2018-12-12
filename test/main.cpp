f69966027bb1029922f8bb6bc93c118b32278985
Varun Sharma <varunsha@andrew.cmu.edu>
2018-12-12 02:45:21 -0500

    add dbg prints

--- test/main.cpp
+++ test/main.cpp
@@ -3,8 +3,19 @@
 #include <assert.h>
 #include <stdlib.h>
 
+#include "sequential_hashmap.h"
 #include "concurrent_hashmap.h"
 #include "concurrent_tm_hashmap.h"
+
+// #define DEBUG // uncomment this line to enable debugging
+
+#ifdef DEBUG
+/* When debugging is enabled, these form aliases to useful functions */
+#define dbg_printf(...) printf(__VA_ARGS__)
+#else
+/* When debugging is disnabled, no code gets generated for these */
+#define dbg_printf(...)
+#endif
 
 
 void test_distinct_put_get(ConcurrentMap& map) {
@@ -12,10 +23,10 @@ void test_distinct_put_get(ConcurrentMap& map) {
 
   for (int iter = 0; iter < 2; iter++) {
     for (uint64_t i = 0; i < 50; i++) {
-      printf("Inserting (%lu, %lu)\n", i, i + 50);
+      dbg_printf("Inserting (%lu, %lu)\n", i, i + 50);
       map.put(i, i + 50);
       assert(i + 50 == map.get(i));
-      printf("Inserted (%lu, %lu)\n", i, i + 50);
+      dbg_printf("Inserted (%lu, %lu)\n", i, i + 50);
       assert(map.getSize() == i + 1);
     }
 
@@ -23,18 +34,18 @@ void test_distinct_put_get(ConcurrentMap& map) {
 
     for (uint64_t i = 0; i < 50; i++) {
       assert(map.getSize() == 50 - i);
-      printf("Getting (%lu, value)\n", i);
+      dbg_printf("Getting (%lu, value)\n", i);
       assert(map.containsKey(i));
       uint64_t value = map.get(i);
       assert(value == i + 50);
-      printf("Got (%lu, %lu)\n", i, value);
+      dbg_printf("Got (%lu, %lu)\n", i, value);
       assert(map.remove(i) == true);
-      printf("Removed (%lu, %lu)\n", i, value);
+      dbg_printf("Removed (%lu, %lu)\n", i, value);
       assert(!map.containsKey(i));
     }
   }
 
-  printf("%s passed!\n", __FUNCTION__);
+  dbg_printf("%s passed!\n", __FUNCTION__);
 }
 
 void test_same_put_get(ConcurrentMap& map) {
@@ -43,9 +54,9 @@ void test_same_put_get(ConcurrentMap& map) {
 
   for (int iter = 0; iter < 2; iter++) {
     for (uint64_t i = 0; i < 50; i++) {
-      printf("Inserting (%lu, %lu)\n", 0UL, i);
+      dbg_printf("Inserting (%lu, %lu)\n", 0UL, i);
       map.put(0, i);
-      printf("Inserted (%lu, %lu)\n", 0UL, i);
+      dbg_printf("Inserted (%lu, %lu)\n", 0UL, i);
       assert(map.getSize() == 1);
     }
 
@@ -54,13 +65,13 @@ void test_same_put_get(ConcurrentMap& map) {
     for (uint64_t i = 0; i < 50; i++) {
       if (i == 0) {
         assert(map.getSize() == 1);
-        printf("Getting (%lu, value)\n", i);
+        dbg_printf("Getting (%lu, value)\n", i);
         assert(map.containsKey(i));
         uint64_t value = map.get(i);
         assert(value == 49);
-        printf("Got (%lu, %lu)\n", i, value);
+        dbg_printf("Got (%lu, %lu)\n", i, value);
         assert(map.remove(i) == true);
-        printf("Removed (%lu, %lu)\n", i, value);
+        dbg_printf("Removed (%lu, %lu)\n", i, value);
       }
 
       assert(map.getSize() == 0);
@@ -68,7 +79,7 @@ void test_same_put_get(ConcurrentMap& map) {
     }
   }
 
-  printf("%s passed!\n", __FUNCTION__);
+  dbg_printf("%s passed!\n", __FUNCTION__);
 }
 
 int main(int argc, char const *argv[])
@@ -78,20 +89,24 @@ int main(int argc, char const *argv[])
     numBuckets = atoi(argv[1]);
   }
 
-  printf("%d, %s\n", argc, argv[0]);
-  printf("numBuckets=%lu\n", numBuckets);
+  dbg_printf("%d, %s\n", argc, argv[0]);
+  dbg_printf("numBuckets=%lu\n", numBuckets);
 
+  SequentialHashMap seq_map1 = SequentialHashMap(numBuckets);
+  SequentialHashMap seq_map2 = SequentialHashMap(numBuckets);
   ConcurrentHashMapBucketLock map1 = ConcurrentHashMapBucketLock(numBuckets);
   ConcurrentHashMapBucketLock map2 = ConcurrentHashMapBucketLock(numBuckets);
-  ConcurrentHashMapTransactionalMemory map3 = ConcurrentHashMapTransactionalMemory(numBuckets);
-  ConcurrentHashMapTransactionalMemory map4 = ConcurrentHashMapTransactionalMemory(numBuckets);
+  // ConcurrentHashMapTransactionalMemory map3 = ConcurrentHashMapTransactionalMemory(numBuckets);
+  // ConcurrentHashMapTransactionalMemory map4 = ConcurrentHashMapTransactionalMemory(numBuckets);
 
+  test_distinct_put_get(seq_map1);
+  test_same_put_get(seq_map2);
 
-  // test_distinct_put_get(map1);
-  // test_same_put_get(map2);
+  test_distinct_put_get(map1);
+  test_same_put_get(map2);
 
-  test_distinct_put_get(map3);
-  test_same_put_get(map4);
+  // test_distinct_put_get(map3);
+  // test_same_put_get(map4);
 
 
   return 0;
