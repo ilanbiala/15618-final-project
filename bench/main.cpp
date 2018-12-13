1aa734e330dbcb020a989916fe8503d56598ee98
Ilan Biala <ilan.biala@gmail.com>
2018-12-12 23:38:48 -0500

    Add code for benchmark

---
+++ bench/main.cpp
@@ -0,0 +1,135 @@
+#include <cstdio>
+#include <cstdlib>
+#include <stdint.h>
+#include <pthread/pthread.h>
+
+#include <algorithm>
+#include <random>
+#include <vector>
+
+#include "../benchmark/include/benchmark/benchmark.h"
+
+#include "concurrent_map.h"
+#include "concurrent_hashmap.h"
+
+// static void CustomArguments(benchmark::internal::Benchmark* b) {
+//   for (int i = 1; i <= 128; i *= 2) {
+//     for (int j = 128; j <= 16384; j *= 2) {
+//       b->Args({i, j});
+//     }
+//   }
+// }
+
+class HashMapFixture : public benchmark::Fixture {
+public:
+  ConcurrentMap *map_ptr;
+  std::vector<uint64_t> keys;
+  pthread_mutex_t mutex;
+  pthread_barrier_t barrier;
+};
+
+BENCHMARK_DEFINE_F(HashMapFixture, RandomRead)(benchmark::State& state) {
+  std::vector<uint64_t> local_keys;
+  auto rng = std::default_random_engine {};
+  // Seed based on thread index so each thread gets a different generator
+  rng.seed(state.thread_index);
+
+  if (state.thread_index == 0) {
+    // pthread_mutex_init(&mutex, NULL);
+    map_ptr = new ConcurrentHashMapBucketLock(state.range(0));
+
+    for (int64_t i = 0; i < state.range(1); i++) {
+      uint64_t k = rand();
+      uint64_t v = rand();
+      map_ptr->put(k, v);
+      keys.push_back(k);
+    }
+  }
+
+  if (pthread_barrier_wait(&barrier) != 0) {
+    printf("ERROR: pthread_barrier_wait() failed!\n");
+    exit(1);
+  }
+
+  // Copy and shuffle thread's access pattern
+  local_keys = keys;
+  std::shuffle(std::begin(local_keys), std::end(local_keys), rng);
+
+  printf("size(local_keys)=%lu\n", local_keys.size());
+
+  for (auto _ : state) {
+    // pthread_mutex_lock(&mutex);
+    // pthread_mutex_unlock(&mutex);
+    for (uint64_t k : local_keys) {
+      benchmark::DoNotOptimize(map_ptr->get(k));
+    }
+  }
+
+  local_keys.clear();
+
+  if (state.thread_index == 0) {
+    keys.clear();
+  }
+}
+
+BENCHMARK_REGISTER_F(HashMapFixture, RandomRead)
+  // ->Apply(CustomArguments);
+  ->RangeMultiplier(2)
+  ->Ranges({{1, 128}, {128, 16384}})
+  ->UseRealTime()
+  ->DenseThreadRange(1, 4, 1);
+
+// static void BM_HashMapRandomRead(benchmark::State& state) {
+//   // ConcurrentHashMapBucketLock map = ConcurrentHashMapBucketLock(state.range(0));
+//   // std::vector<std::vector<uint64_t>> keys(state.threads);
+//   std::vector<uint64_t> keys;
+//   auto rng = std::default_random_engine {};
+
+//   // Setup code
+//   if (state.thread_index == 0) {
+
+//     pthread_mutex_init(&mutex, NULL);
+//     map = ConcurrentHashMapBucketLock(state.range(0));
+//     for (int64_t i = 0; i < state.range(1); i++) {
+//       uint64_t k = rand();
+//       uint64_t v = rand();
+//       map.put(k, v);
+//       keys.push_back(k);
+//       // for (int i = 0; i < state.threads; i++) {
+//         // keys[i].push_back(k);
+//       // }
+//     }
+//   }
+
+//   printf("mutex_addr=%p\n", (void*) &mutex);
+
+//   // Shuffle every thread's access pattern
+//   // for (int i = 0; i < state.threads; i++) {
+//     // std::shuffle(std::begin(keys[i]), std::end(keys[i]), rng);
+//   // }
+
+//   for (auto _ : state) {
+//     pthread_mutex_lock(&mutex);
+//     pthread_mutex_unlock(&mutex);
+//     // for (uint64_t k : keys) {
+//     // for (uint64_t k : keys[state.thread_index]) {
+//       // benchmark::DoNotOptimize(map.get(k));
+//     // }
+//   }
+
+//   // for (int i = 0; i < state.threads; i++) {
+//   //   keys[i].clear();
+//   // }
+// }
+// // Register the function as a benchmark
+// // numBuckets is 1st arg, numElems is 2nd
+// BENCHMARK(BM_HashMapRandomRead)
+//   ->RangeMultiplier(2)
+//   ->Ranges({{1, 128}, {128, 16384}})
+//   ->UseRealTime()
+//   // ->Unit(benchmark::kMicrosecond)
+//   ->ThreadRange(1, 2);
+//   // ->ThreadPerCpu()
+//   // ->Repetitions(3);
+
+BENCHMARK_MAIN();
